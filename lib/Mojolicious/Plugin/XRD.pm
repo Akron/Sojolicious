b7000f1743cf2753b7328ae0dc1c0b01d306f527
Nils Diewald <nils@diewald-online.de>
2013-02-20 13:47:24 +0100

    Improve callback plugin

--- lib/Mojolicious/Plugin/XRD.pm
+++ lib/Mojolicious/Plugin/XRD.pm
@@ -2,13 +2,15 @@ package Mojolicious::Plugin::XRD;
 use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::Util qw/quote/;
 
-our $DELEGATE = 'Mojolicious::Plugin::XML::XRD';
-
 # Register Plugin
 sub register {
   my ($plugin, $mojo) = @_;
 
-  $mojo->types->type(jrd => 'application/json');
+  # Add types
+  for ($mojo->types) {
+    $_->type(jrd => 'application/jrd+json');
+    $_->type(xrd => 'application/xrd+xml');
+  };
 
   # Add 'render_xrd' helper
   $mojo->helper(
@@ -16,7 +18,7 @@ sub register {
       my ($c, $xrd, $res) = @_;
 
       # Define xrd or jrd
-      $c->stash('format' => $c->param('format')) unless $c->stash('format');
+      $c->stash('format' => scalar $c->param('format')) unless $c->stash('format');
 
       # Add CORS header
       $c->res->headers->header('Access-Control-Allow-Origin' => '*');
@@ -24,7 +26,7 @@ sub register {
       my $status = 200;
 
       # Not found
-      if (!defined $xrd) {
+      unless (defined $xrd) {
 	$status = 404;
 	$xrd = $c->new_xrd;
 	$xrd->add(Subject => $res);
@@ -33,9 +35,12 @@ sub register {
       # rel parameter
       elsif ($c->param('rel')) {
 	$xrd = $c->new_xrd($xrd->to_xml);
-	my @rel = split(/\s+/, $c->param('rel'));
-	my $rel = 'Link:' . join(':', map { 'not([rel=' . quote ($_) . '])'} @rel);
-	$xrd->find($rel)->each(sub{ $_->replace('') });
+
+	# Create CSS selector for unwanted relations
+	my $rel = 'Link:' . join(':', map { 'not([rel=' . quote($_) . '])'} $c->param('rel'));
+
+	# Delete all unwanted relations
+	$xrd->find($rel)->pluck('remove');
       };
 
       # content negotiation
@@ -50,7 +55,7 @@ sub register {
 	  data   => $xrd->to_json,
 	  format => 'jrd'
 	)},
-	any  => sub { $c->render(
+	any => sub { $c->render(
 	  status => $status,
 	  data   => $xrd->to_pretty_xml,
 	  format => 'xrd'
@@ -58,8 +63,9 @@ sub register {
       );
     });
 
+  # Add new_xrd helper
   unless (exists $mojo->renderer->helpers->{'new_xrd'}) {
-    $mojo->plugin('XML' => {
+    $mojo->plugin(XML => {
       new_xrd => ['XRD']
     });
   };
@@ -75,6 +81,7 @@ __END__
 
 Mojolicious::Plugin::XRD - Plugin for rendering XRD documents
 
+
 =head1 SYNOPSIS
 
   # Mojolicious
@@ -83,19 +90,20 @@ Mojolicious::Plugin::XRD - Plugin for rendering XRD documents
   my $xrd = $self->new_xrd;
   $self->render_xrd($xrd);
 
+
 =head1 DESCRIPTION
 
 L<Mojolicious::Plugin::XRD> is a plugin to support
-Extensible Resource Descriptor (XRD) documents
-(see L<Specification|http://docs.oasis-open.org/xri/xrd/v1.0/xrd-1.0.html>),
-that where created using L<Mojolicious::Plugin::XML::XRD>.
-Additionally it supports the "rel" parameter of the
+L<Extensible Resource Descriptor|http://docs.oasis-open.org/xri/xrd/v1.0/xrd-1.0.html> documents,
+that where created using L<MojoX::XML::XRD>.
+
+Additionally it supports the C<rel> parameter of the
 L<WebFinger|https://datatracker.ietf.org/doc/draft-jones-appsawg-webfinger/>
 Specification.
 
 =head1 METHODS
 
-=head2 C<register>
+=head2 register
 
   # Mojolicious
   $app->plugin('XRD');
@@ -108,7 +116,7 @@ Called when registering the plugin.
 
 =head1 HELPERS
 
-=head2 C<render_xrd>
+=head2 render_xrd
 
   # In Controllers
   $self->render_xrd( $xrd );
@@ -119,26 +127,29 @@ in C<xml> or in C<json> notation, depending on the request.
 If an XRD object is empty, it renders a 404 error
 and accepts a second parameter as the subject of the 404.
 
-=head2 C<new_hostmeta>
+
+=head2 new_xrd
 
   # In Controller:
   my $xrd = $self->new_xrd;
 
-The helper C<new_xrd> returns a new L<Mojolicious::Plugin::XML::XRD>
-object.
+Returns a new L<MojoX::XML::XRD> object.
 
 =head1 DEPENDENCIES
 
 L<Mojolicious>,
+L<MojoX::XML>,
 L<Mojolicious::Plugin::XML>.
+
 
 =head1 AVAILABILITY
 
   https://github.com/Akron/Sojolicious
 
+
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011-2012, Nils Diewald.
+Copyright (C) 2011-2013, L<Nils Diewald|http://nils-diewald.de>.
 
 This program is free software, you can redistribute it
 and/or modify it under the same terms as Perl.
