3a42a08a69577865297e38c3b9110e979cdd3df9
Akron <akron@sojolicio.us>
2013-03-03 23:45:31 +0100

    Minor changes

---
+++ lib/Sojolicious/Command/sojo_speedup.pm
@@ -0,0 +1,12 @@
+# Idea:
+- Check for all libraries missing for a speedup
+- Trigger a cpan installation (like perl Makefile)
+- May need sudo rights
+
+  - Alien::GMP!
+
+- Better - start cpanm for these things!
+
+- Maybe a command is not such a good idea.
+  Better make it a distribution:
+  Sojolicious::SpeedUp
